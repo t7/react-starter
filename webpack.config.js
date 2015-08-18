1fd872cba19762fae551ae1053457fe39c745781
mundi morgado <mundi@tandemseven.com>
2015-08-18 13:08:20 -0700

    updates

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 var webpack = require('webpack')
+var ExtractTextPlugin = require('extract-text-webpack-plugin')
+
 module.exports = {
   devServer: {
     contentBase: './build',
@@ -21,12 +23,23 @@ module.exports = {
       test: /\.js$/,
       exclude: /node_modules/,
       loader: 'babel-loader'
-    }, {
-      test: /\.css$/,
-      loader: 'style!css'
-    }]
+    }, { test: /\.css$/, loader: ExtractTextPlugin.extract('style-loader', 'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss-loader') },
+    { test: /\.svg$/, loader: 'url-loader?limit=10000&mimetype=image/svg+xml' },
+    { test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192&name=[name]_[sha512:hash:base64:7].[ext]'}
+
+  ]},
+
+  postcss: [
+    require('autoprefixer-core')
+  ],
+
+  resolve: {
+    modulesDirectories: ['node_modules', 'components']
   },
+
   plugins: [
+    new ExtractTextPlugin('bundle.css', { allChunks: true }),
     new webpack.NoErrorsPlugin()
   ]
 }
+
