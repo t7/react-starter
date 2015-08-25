66ad4cfc3d33cfbfc795e5bbcdc29e4583dea46a
Nathan Logan <nathanlogan@gmail.com>
2015-08-25 13:46:03 -0600

    Adding to the login component

--- source/components/login/index.js
+++ source/components/login/index.js
@@ -4,7 +4,13 @@ import styles from './index.css'
 let Login = React.createClass({
 
   render () {
-    return (<div className={styles.foo}>Welcome to login</div>)
+    return (
+      <div className={styles.foo}>
+        <label>Username: <input type='text' /></label>
+        <label>Password: <input type='password' /></label>
+        <input type='submit' value='Login' />
+      </div>
+    )
   }
 })
 
