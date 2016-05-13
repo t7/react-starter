cb19e2db0107a9cbb5db0316d5ea14f1387e33b7
Nathan Smith <nathan@sonspring.com>
2016-05-13 11:58:24 -0500

    Create _path.js

---
+++ source/utils/_path.js
@@ -0,0 +1,30 @@
+// Dependencies.
+import { isArray } from 'lodash'
+
+// Helper to generate "#" URL.
+function path (x) {
+  if (!x) {
+    // Remove cache buster.
+    return window.location.hash.split('?')[0]
+  }
+
+  if (arguments.length > 1) {
+    // Convert to array.
+    x = Array.prototype.slice.call(arguments)
+  }
+
+  if (isArray(x)) {
+    // Convert to string.
+    x = x.join('/')
+  }
+
+  x = '' + x
+  x = x.replace(/#/g, '')
+  x = '#/' + x
+  x = x.replace(/\/+/g, '/')
+
+  return x
+}
+
+// Expose function.
+export default path
