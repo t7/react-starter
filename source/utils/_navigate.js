0d4e102c468ce1b110908395d729d26058cf1554
Nathan Smith <nathan@sonspring.com>
2016-05-13 11:58:36 -0500

    Update _navigate.js

--- source/utils/_navigate.js
+++ source/utils/_navigate.js
@@ -1,10 +1,28 @@
+// Dependencies.
+import { isArray } from 'lodash'
+
 // Helper for page navigation.
-function navigate (path) {
-  if (path === '/') {
-    path = ''
+function navigate (x) {
+  if (!x) {
+    return
   }
 
-  window.location.hash = '/' + path
+  if (arguments.length > 1) {
+    // Convert to array.
+    x = Array.prototype.slice.call(arguments)
+  }
+
+  if (isArray(x)) {
+    // Convert to string.
+    x = x.join('/')
+  }
+
+  x = '' + x
+  x = x.replace(/#/g, '')
+  x = '#/' + x
+  x = x.replace(/\/+/g, '/')
+
+  window.location.hash = x
 }
 
 // Expose function.
